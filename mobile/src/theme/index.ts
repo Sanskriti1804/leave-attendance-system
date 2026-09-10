842487a0dae29ffa390fa3c00d78fe7a4fabec35
Sanskriti1804 <sanskriti182004@gmail.com>
2026-09-10 14:30:57 +0530

    ui - login screen w pass button

---
+++ mobile/src/theme/index.ts
@@ -0,0 +1,12 @@
+import { colors } from './colors';
+import { spacing, radii } from './spacing';
+import { typography } from './typography';
+
+export const theme = {
+  colors,
+  spacing,
+  radii,
+  typography,
+};
+
+export type Theme = typeof theme;
